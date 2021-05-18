c026a9289589ef820fe10094794992c38c6aca76
Carlos Alexandre <carlos.alexandre@nos.pt>
2021-05-18 17:27:50 +0100

    wip

--- dns.tf
+++ dns.tf
@@ -1,10 +1,10 @@
 ## 3.1. Descomentar para ativar o modulo de DNS
-# module "dns" {
-#   source     = "./modules/dns"
-#   project_id = data.google_project.this.id
-#   prefix     = local.prefix
-# }
+module "dns" {
+  source     = "./modules/dns"
+  project_id = data.google_project.this.id
+  prefix     = local.prefix
+}
 
-# output "fqdn" {
-#   value = module.dns.fqdn
-# }
+output "fqdn" {
+  value = module.dns.fqdn
+}
--- modules/gke/kubeconfig.tf
+++ modules/gke/kubeconfig.tf
@@ -1,11 +1,11 @@
 ## 2.3. Utilizar modules dentro de modules
-# module "kubeconfig" {
-#   source     = "../kubeconfig"
-#   project_id = data.google_project.this.id
-#   gke_name   = google_container_cluster.default.name
-#   region     = google_container_cluster.default.location
-# }
+module "kubeconfig" {
+  source     = "../kubeconfig"
+  project_id = data.google_project.this.id
+  gke_name   = google_container_cluster.default.name
+  region     = google_container_cluster.default.location
+}
 
-# output "gke_kubeconfig" {
-#   value = module.kubeconfig.gke_kubeconfig
-# }
+output "gke_kubeconfig" {
+  value = module.kubeconfig.gke_kubeconfig
+}
