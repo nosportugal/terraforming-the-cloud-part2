d9b086fe605033e099334df9dea598b7cc33c064
Carlos Alexandre <carlos.alexandre@nos.pt>
2021-05-18 17:21:37 +0100

    Revert "added NAT settings"
    This reverts commit a75b2c34dee43497a9657b97adb4fd48c7e92769.

--- modules/gke copy/output.tf
+++
@@ -1,7 +0,0 @@
-output "gke_default_node_version" {
-  value = google_container_cluster.default.node_version
-}
-
-output "gke_default_endpoint" {
-  value = google_container_cluster.default.endpoint
-}
