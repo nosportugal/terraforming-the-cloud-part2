2f51ab4519f1b284fd9eb04f63962dc3d3675a77
Pedro Lourenço <64209266+pfilourenco@users.noreply.github.com>
2021-05-19 09:41:31 +0100

    wip

--- gke.tf
+++ gke.tf
@@ -1,11 +1,11 @@
 # ## 2.2. GKE module
-module "gke" {
-  source     = "./modules/gke"
-  project_id = data.google_project.this.id
-  region     = var.region
-  prefix     = local.prefix
-  vpc_subnet = google_compute_subnetwork.gke.self_link
-}
+#module "gke" {
+#  source     = "./modules/gke"
+#  project_id = data.google_project.this.id
+#  region     = var.region
+#  prefix     = local.prefix
+#  vpc_subnet = google_compute_subnetwork.gke.self_link
+#}
 
 ## 2.3. Descomentar este output apenas no passo 2.3
 # output "gke_kubeconfig_usage" {
