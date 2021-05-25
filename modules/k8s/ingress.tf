25ce97ec24054572141f6e951e22e9d04bb50019
Pedro Lourenço <pedro.flourenco@nos.pt>
2021-05-25 20:06:26 +0100

    add gcp managed certs on ingress

--- modules/k8s/ingress.tf
+++ modules/k8s/ingress.tf
@@ -1,4 +1,20 @@
 ## 3.3 - Deploy de ingress
+# data "kubectl_path_documents" "hipster_cert" {
+#   pattern = "templates/gcp-managed-cert.yaml"
+#   vars = {
+#     fqdn = local.fqdn
+#   }
+# }
+
+# resource "kubectl_manifest" "hipster_cert" {
+#   count     = length(data.kubectl_path_documents.hipster_cert.documents)
+#   yaml_body = element(data.kubectl_path_documents.hipster_cert.documents, count.index)
+
+#   depends_on = [
+#     kubectl_manifest.hipster_workloads
+#   ]
+# }
+#
 # data "kubectl_path_documents" "hipster_ingress" {
 #   pattern = "templates/hipster-ingress-template.yaml"
 #   vars = {
