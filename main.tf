0acde340954f75d9cd1888174606cf048d692b72
Carlos Alexandre <carlos.alexandre@nos.pt>
2021-05-25 16:23:39 +0100

    updated google provider version requirement

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.58.0"
+      version = ">= 3.69.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
