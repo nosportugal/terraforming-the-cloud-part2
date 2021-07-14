e2e6b6efce47c04d60497f8d1dd9ba930b476c2e
Carlos Alexandre <carlos.alexandre@nos.pt>
2021-07-14 12:26:02 +0100

    adicionei uma exclamacao

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
   }
 }
 
-## referenciar um recurso já existente
+## referenciar um recurso já existente!
 ## ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project
 data "google_project" "this" {
   project_id = var.project_id
