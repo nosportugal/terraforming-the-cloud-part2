622a4151329639d32275457339c7c5f9444438d4
Carlos Alexandre <carlos.alexandre@nos.pt>
2022-07-01 14:37:24 +0100

    fixed comment

--- k8s.tf
+++ k8s.tf
@@ -4,7 +4,7 @@
 #   source     = "./modules/k8s"
 #   project_id = data.google_project.this.id
 #   ## 3.2 - Passar o FQDN do DNS para o modulo
-#   fqdn                    = module.dns.fqdn
+#   #fqdn                    = module.dns.fqdn
 #   sa_gke_dns              = var.sa_gke_dns
 
 #   depends_on = [
