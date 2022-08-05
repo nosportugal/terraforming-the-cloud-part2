659d2df172187018e376cfe282c0a0f0b1a4c6da
Carlos Alexandre <carlos.alexandre@nos.pt>
2022-08-05 20:50:37 +0100

    converted to autopilot

--- k8s.cloudretro.tf
+++ k8s.cloudretro.tf
@@ -2,16 +2,16 @@
 ## ⭐⭐ hidden bonus ⭐⭐
 ##
 ## Apenas descomenta quando acabares a demo toda
-module "cloudretro" {
-  source     = "./modules/cloudretro"
-  fqdn       = module.dns.fqdn
+# module "cloudretro" {
+#   source     = "./modules/cloudretro"
+#   fqdn       = module.dns.fqdn
 
-  depends_on = [
-    module.gke,
-    module.dns
-  ]
-}
+#   depends_on = [
+#     module.gke,
+#     module.dns
+#   ]
+# }
 
-output "cloudretro_endpoint" {
-  value = module.cloudretro.endpoint
-}
+# output "cloudretro_endpoint" {
+#   value = module.cloudretro.endpoint
+# }
--- k8s.hipster.tf
+++ k8s.hipster.tf
@@ -1,14 +1,14 @@
 
 ## 2.4. K8s workloads
-module "hipster" {
-  source     = "./modules/hipster-demo"
-  project_id = data.google_project.this.id
+# module "hipster" {
+#   source     = "./modules/hipster-demo"
+#   project_id = data.google_project.this.id
 
-  ## 3.3 - Passar o FQDN do DNS para o modulo
-  fqdn            = module.dns.fqdn
-  ingress_enabled = true
+#   ## 3.3 - Passar o FQDN do DNS para o modulo
+#   # fqdn            = module.dns.fqdn
+#   # ingress_enabled = true
 
-  depends_on = [
-    module.gke
-  ]
-}
+#   depends_on = [
+#     module.gke
+#   ]
+# }
